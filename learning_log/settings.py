33b89861c1249cd7e39269629b9a4c6abe1b2686
xpinchx <dougwelch@gmail.com>
2021-05-21 15:36:03 -0500

    Ready for deployment to heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,11 +137,6 @@ BOOTSTRAP3 = {
 }
 
 # Heroku settings
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
-os.makedirs(STATIC_TMP, exist_ok=True)
-os.makedirs(STATIC_ROOT, exist_ok=True)
 
 cwd = os.getcwd()
 if cwd == '/app' or cwd[:4] == '/tmp':
