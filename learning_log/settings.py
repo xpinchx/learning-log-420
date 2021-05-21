159a46e545c2d9fa6dbb3cca3ad2ca339c435aee
xpinchx <dougwelch@gmail.com>
2021-05-21 15:49:02 -0500

    Ready for deployment to heroku.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -137,7 +137,18 @@ BOOTSTRAP3 = {
 }
 
 # Heroku settings
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 cwd = os.getcwd()
 if cwd == '/app' or cwd[:4] == '/tmp':
