9a4fe7464bff356145aaa597752c869db5e3489d
xpinchx <dougwelch@gmail.com>
2021-05-21 18:29:38 -0500

    Something broke, testing.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -21,8 +21,8 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
   """Show a single topic and all its entries"""
-  # topic = Topic.objects.get(id=topic_id)
-  topic = get_object_or_404(Topic, id=topic_id)
+  topic = Topic.objects.get(id=topic_id)
+  # topic = get_object_or_404(Topic, id=topic_id)
   # Make sure the topic belongs to the current user
   if topic.owner != request.user:
     raise Http404
