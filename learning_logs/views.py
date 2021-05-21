282c682d5cd0279b39b69c0a90983c24eeb184ea
xpinchx <dougwelch@gmail.com>
2021-05-21 18:24:10 -0500

    Added custom 404 and 500 error pages.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -21,7 +21,8 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
   """Show a single topic and all its entries"""
-  topic = Topic.objects.get(id=topic_id)
+  # topic = Topic.objects.get(id=topic_id)
+  topic = get_object_or_404(Topic, id=topic_id)
   # Make sure the topic belongs to the current user
   if topic.owner != request.user:
     raise Http404
